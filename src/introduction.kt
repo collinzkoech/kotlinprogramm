6fab287ad3bba5eba94c77eb55ddb021274267cd
collinzkoech <colllinskoech024@gmail.com>
2024-03-11 21:05:06 +0300

    initial  commit

---
+++ src/introduction.kt
@@ -0,0 +1,10 @@
+fun main() {
+    println("collins")
+    println(20)
+    println(20+30)
+    println("emobilis")
+    println("collins")
+    println("name")
+
+
+}
---
+++ src/variables.kt
@@ -0,0 +1,9 @@
+fun main() {
+    var firstname = "collins"
+    firstname = "Isaack"
+    val pi = 3.14
+    var lastname = "koech"
+    var school = "emobilis"
+    println(firstname+ " " +lastname+" "+school )
+    println("My firstname is " +firstname+".I study at eMobilis")
+}
